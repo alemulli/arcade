0c7ee1479d9f7822197b852ae4315cda75bc119e
root <root@DESKTOP-M366OAF.localdomain>
2022-10-06 10:51:13 -0400

    establishing an x y grid and putting the initial snake and apple on the grid

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 let speed;
-let direction = "up";
+let direction = "none";
+let highScore;
+let averageScore;
+let gameState = {
+    apple: [4, 4],
+    snake: {
+        body: [6, 7], 
+    }
+}
+
+let xApple = gameState.apple[0]
+let yApple = gameState.apple[1]
+let xSnake = gameState.snake.body[0]
+let ySnake = gameState.snake.body[1]
 
 ////// Choosing difficulty with the difficulty radio buttons //////
 
@@ -22,6 +35,7 @@ function setDifficulty () {
 difficultySelector.addEventListener("click", setDifficulty)
 
 ////// Establishing the game-grid with the Field Size radio buttons //////
+// Putting the initial starting pos for the snake and apple on the game field //
 
 const gameGridHere = document.getElementById("gameGridHere");
 const fieldSizeSelector = document.getElementById("fieldSize")
@@ -31,11 +45,11 @@ const fieldSizeLarge = document.getElementById("largeField")
 
 function createGameField () {
     if (fieldSizeSmall.checked === true) {
-        let size = 20
+        let size = 10
         
-        for (let i = 0; i <= size; i++) {
+        for (let i = 0; i < size; i++) {
             const newTr = document.createElement("tr");
-            for (let j = 0; j <= size; j++) {
+            for (let j = 0; j < size; j++) {
                 const newTd = document.createElement("td");
                 newTr.appendChild(newTd);
             }
@@ -43,11 +57,14 @@ function createGameField () {
         }
         fieldSizeSmall.disabled=true;
         fieldSizeLarge.disabled=true;
+
+        document.getElementsByTagName("tr")[ySnake].getElementsByTagName('td')[xSnake].classList.add("snake")
+        document.getElementsByTagName("tr")[yApple].getElementsByTagName('td')[xApple].classList.add("apple")
     } else if (fieldSizeLarge.checked === true) {
-        let size = 40;
-        for (let i = 0; i <= size; i++) {
+        let size = 20;
+        for (let i = 0; i < size; i++) {
             const newTr = document.createElement("tr");
-            for (let j = 0; j <= size; j++) {
+            for (let j = 0; j < size; j++) {
                 const newTd = document.createElement("td");
                 newTr.appendChild(newTd);
             }
@@ -55,6 +72,8 @@ function createGameField () {
         }
         fieldSizeSmall.disabled=true;
         fieldSizeLarge.disabled=true;
+        document.getElementsByTagName("tr")[ySnake].getElementsByTagName('td')[xSnake].classList.add("snake")
+        document.getElementsByTagName("tr")[yApple].getElementsByTagName('td')[xApple].classList.add("apple")
     }
 }
 
